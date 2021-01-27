16cadbde3cb4d72a9fe621e0898985513317af69
Aliaksandr Zayats <a.zayats@outlook.com>
2021-01-27 14:04:28 +0300

    updates

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Explosion : MonoBehaviour
 {
     public Vector3 Offset = Vector3.zero;
+    public float ExplosionForce = 10000;
+    public float ExplosionRadius = 10;
+    public int Size = 10;
 
     public GameObject Cube;
 
@@ -12,11 +15,11 @@ public class Explosion : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            for (float x = 0; x < 10; x++)
+            for (float x = 0; x < Size; x++)
             {
-                for (float y = 0; y < 10; y++)
+                for (float y = 0; y < Size; y++)
                 {
-                    for (float z = 0; z < 10; z++)
+                    for (float z = 0; z < Size; z++)
                     {
                         var cube = Instantiate(Cube, new Vector3(x, y + 0.5f, z) + Offset, Quaternion.identity);
                         cube.GetComponent<MeshRenderer>().material = Materials[Random.Range(0, Materials.Length - 1)];
@@ -26,14 +29,14 @@ public class Explosion : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
-            var position = new Vector3(5f, 5.5f, 5f) + Offset;
-            var colliders = Physics.OverlapSphere(position, 10);
+            var position = new Vector3(Size / 2, Size / 2 + 0.5f, Size / 2) + Offset;
+            var colliders = Physics.OverlapSphere(position, ExplosionRadius);
             foreach (var collider in colliders)
             {
                 var rb = collider.GetComponent<Rigidbody>();
                 if (rb != null)
                 {
-                    rb.AddExplosionForce(10000 * (1 / Time.timeScale), position, 10);
+                    rb.AddExplosionForce(ExplosionForce * (1 / Time.timeScale), position, ExplosionRadius);
                 }
             }
         }
